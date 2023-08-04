b111647552ef2e222faed7d228148a225f02339b
pwalendz <122270630+pwalendz@users.noreply.github.com>
2023-08-04 13:58:03 +0300

    done

---
+++ src/main/java/com/example/phase5proj/Phase5projApplication.java
@@ -0,0 +1,13 @@
+package com.example.phase5proj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = "com")
+public class Phase5projApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Phase5projApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/phase5proj/Phase5projApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.phase5proj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Phase5projApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
